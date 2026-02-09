48e03b09acab39aed0cdc722a3c97874d563881e
Emanuele De Angelis <emanuele.de.angelis30@gmail.com>
2026-02-09 19:33:56 +0100

    added a few lazy configs

---
+++ configs/lazy_stb_config.pl
@@ -0,0 +1,16 @@
+:- set_lopt(learning_mode(brave)).
+
+:- set_lopt(folding_mode(lazy)).
+
+:- set_lopt(folding_selection(any)).
+
+%:- set_lopt(folding_space(all)).
+:- set_lopt(folding_space(bk)).
+
+:- set_lopt(asm_intro(relto)).
+
+:- set_lopt(check_ic).
+
+:- set_lopt(verbosity(info)).
+:- set_lopt(clingo_time_limit(600)).
+:- set_lopt(semantics(stb)).
