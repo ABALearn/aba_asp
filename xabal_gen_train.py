d81a601df7b21094e1372459e65195f44b29bb83
Emanuele De Angelis <emanuele.de.angelis30@gmail.com>
2026-04-24 16:55:53 +0200

    added a few tmp results

--- xabal_gen_train.py
+++ xabal_gen_train.py
@@ -5,15 +5,38 @@ import datetime
 import sys
 import glob
 
-sem = ["stb"]#, "adm", "com", "grd", "prf"]
-time_limit = 300
+sem = ["stb", "adm", "com", "grd", "prf"]
+time_limit = 60
 config="configs/lazy_"
 goals = glob.glob("./lib/abagen/abalp/*.bk.*.pl")
+basedir = os.path.dirname(goals[0])
 
-for f in goals:
-  for s in sem:
-      cmd = "./cmdrunner \'swipl -g halt aba_asp.pl " + config + s + "_config.pl " + f + "\' " + str(time_limit)
-      #subprocess.call([cmd], shell=True)
-      print(cmd)
+for s in sem:
+  sdir = basedir + "/" + s
+  if os.path.exists(sdir):
+    shutil.rmtree(sdir)
+  os.makedirs(sdir)
+  for f in goals:
+    cmd = "./cmdrunner \'swipl -g halt aba_asp.pl " + config + s + "_config.pl " + f + "\' " + str(time_limit)
+    subprocess.call([cmd], shell=True)
+    learnt_abaf = f.replace(".pl",".sol.aba")
+    if os.path.exists(learnt_abaf):
+      shutil.move(learnt_abaf, sdir + "/")
+      learnt_abaf_asp = f.replace(".pl",".sol.asp")
+      if os.path.exists(learnt_abaf_asp):
+        shutil.move(learnt_abaf_asp, sdir + "/")
+      learnt_abaf_chk = f.replace(".pl",".sol_chk.asp") 
+      if os.path.exists(learnt_abaf_chk):  
+        shutil.move(learnt_abaf_chk, sdir + "/")  
+    else:
+      with open('aba_asp.csv', "a") as stats:
+        stats.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S,"))
+        stats.write(os.path.basename(f) + ",")
+        stats.write(s + ",")
+        stats.write("\'\\N\',")
+        stats.write("\'\\N\',")
+        stats.write("\'\\N\',")
+        stats.write("\'\\N\',")
+        stats.write("to\n")
 
 sys.exit()
